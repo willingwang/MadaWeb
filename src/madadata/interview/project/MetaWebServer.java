e3c90d6e878d0afad1770091d0d610ff7e3ff4ad
willingwang <505234811@qq.com>
2016-03-02 16:06:28 +0800

    debugging

--- src/madadata/interview/project/MetaWebServer.java
+++ src/madadata/interview/project/MetaWebServer.java
@@ -2,6 +2,7 @@ package madadata.interview.project;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.HashMap;
@@ -19,7 +20,9 @@ public class MetaWebServer {
 //		System.out.println(WEBROOT);
 //		System.out.println(System.getProperty("user.dir"));
 		setContentType();
-		ServerSocket serverSocket = new ServerSocket(10002);
+		ServerSocket serverSocket = new ServerSocket(10006);
+		serverSocket.setReuseAddress(false);
+
 		System.out.println("MetaWebServer is running on port: " + serverSocket.getLocalPort());
 		while(true) {
 			Socket socket = serverSocket.accept();
@@ -28,7 +31,7 @@ public class MetaWebServer {
 			Request request = new Request(input);	
 			request.parse();
 			Response response = new Response(request, output);
-			ExecutorService es = Executors.newCachedThreadPool();
+			ExecutorService es = Executors.newFixedThreadPool(32);
 			es.submit(new RequestHandler(request,response));
 		}
 	}
